20c2d82c92be5225d1d86627e07a2e4dd22c8fee
axlchr12 <axelchristopher12@gmail.com>
2026-03-17 01:04:12 +0700

    fix: WIP TitleSelectionList

--- src/pages/content/TitleSelectionList.tsx
+++ src/pages/content/TitleSelectionList.tsx
@@ -1,7 +1,21 @@
 import { UseApp } from '../../context';
 
 export const TitleSelectionList = () => {
-  const { partnerId } = UseApp();
+  const {} = UseApp();
 
-  return <>test cenah {partnerId}</>;
+  return (
+    <div className="grid grid-cols-4 px-20 gap-2">
+      {Array.from({ length: 12 }).map((data, index) => {
+        return (
+          <div className="">
+            <img
+              className=""
+              src="https://pbs.twimg.com/media/HDg8hslaoAAwf41?format=jpg&name=small"
+              key={index}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
